44338ffd95fe7e0d659bf2f743fcb1e1774aebe4
InjuredMuffin2 <k00276349@student.tus.ie>
2023-12-01 00:27:52 +0000

    Preparing for inventory stuff

--- Assets/00-MyAssets/Scripts/GameManager.cs
+++ Assets/00-MyAssets/Scripts/GameManager.cs
@@ -1,15 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class InventoryItemData
+{
+    public ItemSO itemInSlot;
+    private int quantity;
+}
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public GameObject cameraPoint;
     public GameObject player;
 
+    public GameObject cameraPoint;
+
     public Sprite missingSprite;
+    public Color common, uncommon, rare, epic, legendary, mythic, otherworldly;
+
+    private List<InventoryItemData> _inventorySlots = new List<InventoryItemData>();
+    private bool _freeSlotsAvailable;
+    private List<int> _freeSlotIndexes;
 
     private void Awake()
     {
@@ -26,5 +40,35 @@ public class GameManager : MonoBehaviour
         cameraPoint = GameObject.Find("PlayerFollowCamera");
     }
 
-    public Color common, uncommon, rare, epic, legendary, mythic, otherworldly;
+    public void AddItemToInventory(ItemSO itemToAdd)
+    {
+        CheckForFreeSlots();
+
+        if (_freeSlotsAvailable)
+        {
+            if (!itemToAdd.isStackable)
+            {
+                // Add the item to a free inventory slot
+                CheckForFreeSlots(); // or directly remove the index of the slot from the list
+            }
+            else
+            {
+                // Look through inventory list to see if the item is there,
+                // if found increnemt the count by 1
+                // if not found, add it to a free slot
+                CheckForFreeSlots(); // or directly remove the index of the slot from the list
+            }
+        }
+    }
+
+    public void CheckForFreeSlots()
+    {
+        foreach(InventoryItemData inventorySlot in _inventorySlots)
+        {
+            // check each slot to see if it is free
+            // if slot is free and not in free slot list, add index to free slot list
+            // if slot is not free and in the free slot list, remove its idex from the list
+            // Suggestion 
+        }
+    }
 }
--- Assets/00-MyAssets/Scripts/Item.cs
+++ Assets/00-MyAssets/Scripts/Item.cs
@@ -8,10 +8,10 @@ public class Item : MonoBehaviour
     public ItemSO itemSO;
     public float spriteDisableDistance = 48f, lightDisableDistance = 24f, physicsDisableDistance = 64f;
 
-    private Collider itemCollider;
-    private Rigidbody itemRB;
-    private SpriteRenderer itemSR;
-    private Light itemLight;
+    private Collider _itemCollider;
+    private Rigidbody _itemRB;
+    private SpriteRenderer _itemSR;
+    private Light _itemLight;
 
     public float bobbingHeight = 0.2f;
     public float bobbingSpeed = 2f, bobbingSpeedOffset = 0.5f;
@@ -19,39 +19,39 @@ public class Item : MonoBehaviour
     private void OnEnable()
     {
         // 1. Get components
-        itemCollider = GetComponent<Collider>();
-        itemRB = GetComponent<Rigidbody>();
-        itemSR = GetComponentInChildren<SpriteRenderer>();
-        itemLight = GetComponentInChildren<Light>();
+        _itemCollider = GetComponent<Collider>();
+        _itemRB = GetComponent<Rigidbody>();
+        _itemSR = GetComponentInChildren<SpriteRenderer>();
+        _itemLight = GetComponentInChildren<Light>();
 
         // 2. Set the variables
-        originalY = itemSR.transform.localPosition.y;
+        originalY = _itemSR.transform.localPosition.y;
         bobbingSpeed = Random.Range(bobbingSpeed - bobbingSpeedOffset, bobbingSpeed + bobbingSpeedOffset);
-        itemRB.mass = itemSO.itemMass;
-        if(itemRB.mass < 0.25f) itemRB.mass = 0.25f;
+        _itemRB.mass = itemSO.itemMass;
+        if(_itemRB.mass < 0.25f) _itemRB.mass = 0.25f;
 
         // Since this is in the OnEnable function-
         // this makes sure the starting intensity doesn't get set again-
         // when the item leaves the player's range and comes back
-        if (startingLightIntensity == 0) startingLightIntensity = itemLight.intensity;
+        if (startingLightIntensity == 0) startingLightIntensity = _itemLight.intensity;
 
         // Check if ScriptableObject is assigned
         if (itemSO == null)
         {
             Debug.LogWarning($"ScriptableObject is missing on the item {gameObject.name}!");
-            itemSR.sprite = GameManager.Instance.missingSprite;
-            StartCoroutine(FlashRed(itemLight));
+            _itemSR.sprite = GameManager.Instance.missingSprite;
+            StartCoroutine(FlashRed(_itemLight));
             return;
         }
 
         // Set the sprite                 if true set itemSO's sprite | if false get fallback sprite
-        itemSR.sprite = itemSO.itemSprite != null ? itemSO.itemSprite : GameManager.Instance.missingSprite;
+        _itemSR.sprite = itemSO.itemSprite != null ? itemSO.itemSprite : GameManager.Instance.missingSprite;
 
         // If itemSO is assigned but has no sprite variable assigned... do:
         if (itemSO.itemSprite == null)
         {
             Debug.LogWarning($"Item sprite is missing in the ScriptableObject for the item: {gameObject.name}!");
-            StartCoroutine(FlashRed(itemLight));
+            StartCoroutine(FlashRed(_itemLight));
         }
 
         SetLightColourBasedOnRarity();
@@ -84,7 +84,7 @@ public class Item : MonoBehaviour
 
         directionToCamera = -directionToCamera;
         directionToCamera.y = 0;
-        itemSR.transform.forward = directionToCamera;
+        _itemSR.transform.forward = directionToCamera;
     }
 
     // The item sprite bobs up and down slowly
@@ -95,7 +95,7 @@ public class Item : MonoBehaviour
         float newY = originalY + bobbingHeight * Mathf.Sin(Time.time * bobbingSpeed);
 
         // Update the localPosition of the item
-        itemSR.transform.localPosition = new Vector3(0, newY, 0);
+        _itemSR.transform.localPosition = new Vector3(0, newY, 0);
     }
 
     // for performance reasons, make different components of an item get disabled-
@@ -111,10 +111,10 @@ public class Item : MonoBehaviour
         lightEnabled = distanceToPlayer <= lightDisableDistance;
         physicsEnabled = distanceToPlayer <= physicsDisableDistance;
 
-        if (itemSR != null) itemSR.gameObject.SetActive(spriteEnabled);
-        if (itemLight != null) itemLight.gameObject.SetActive(lightEnabled);
-        if (itemRB != null) itemRB.isKinematic = !physicsEnabled;
-        if (itemCollider != null) itemCollider.enabled = physicsEnabled;
+        if (_itemSR != null) _itemSR.gameObject.SetActive(spriteEnabled);
+        if (_itemLight != null) _itemLight.gameObject.SetActive(lightEnabled);
+        if (_itemRB != null) _itemRB.isKinematic = !physicsEnabled;
+        if (_itemCollider != null) _itemCollider.enabled = physicsEnabled;
     }
 
     // Set light's colour based on the item's rarity from the itemSO
@@ -123,29 +123,29 @@ public class Item : MonoBehaviour
         switch (itemSO.itemRarity)
         {
             case ItemRarity.Common:
-                itemLight.color = GameManager.Instance.common;
+                _itemLight.color = GameManager.Instance.common;
                 break;
             case ItemRarity.Uncommon:
-                itemLight.color = GameManager.Instance.uncommon;
+                _itemLight.color = GameManager.Instance.uncommon;
                 break;
             case ItemRarity.Rare:
-                itemLight.color = GameManager.Instance.rare;
+                _itemLight.color = GameManager.Instance.rare;
                 break;
             case ItemRarity.Epic:
-                itemLight.color = GameManager.Instance.epic;
+                _itemLight.color = GameManager.Instance.epic;
                 break;
             case ItemRarity.Legendary:
-                itemLight.color = GameManager.Instance.legendary;
+                _itemLight.color = GameManager.Instance.legendary;
                 break;
             case ItemRarity.Mythic:
-                itemLight.color = GameManager.Instance.mythic;
+                _itemLight.color = GameManager.Instance.mythic;
                 break;
             case ItemRarity.Otherworldly:
-                itemLight.color = GameManager.Instance.otherworldly;
+                _itemLight.color = GameManager.Instance.otherworldly;
                 break;
             default:
                 Debug.LogWarning($"Item rarity Not Set for {gameObject.name}!");
-                StartCoroutine(FlashRed(itemLight));
+                StartCoroutine(FlashRed(_itemLight));
                 break;
         }
     }
@@ -164,7 +164,7 @@ public class Item : MonoBehaviour
             float normalizedDistance = distanceToPlayer / lightDisableDistance;
 
             // Interpolate the light's intensity from its maximum value to 0
-            itemLight.intensity = Mathf.Lerp(startingLightIntensity, 0, normalizedDistance);
+            _itemLight.intensity = Mathf.Lerp(startingLightIntensity, 0, normalizedDistance);
         }
     }
 }
